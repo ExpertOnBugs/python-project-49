fd67a13ce455ea231adb1ba100f568c1c3eb0a0b
ExpertOnBugs <dmitriy.sam01@mail.ru>
2026-06-03 14:31:29 +0300

    add ruff.toml

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,7 @@
 import prompt
 
+
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    print('Hello, ', name, '!', sep = '')
+    print('Hello, ', name, '!', sep='')
     return name
